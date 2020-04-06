388e7985b3f20c3a01aacf0e641cb0b6666cdfe4
Pranesh Palanisamy <ppranesh5@gmail.com>
2020-04-07 00:10:38 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -23,25 +23,25 @@ function listenToValues(key) {
 
     switch (key) {
         case "w":
-            var w_button = new Audio("/sounds/tom-3.mp3").play();
+            var w_button = new Audio("/drums_band_with_js/sounds/tom-3.mp3").play();
             break;
         case "a":
-            var a_button = new Audio("/sounds/snare.mp3").play();
+            var a_button = new Audio("/drums_band_with_js/sounds/snare.mp3").play();
             break;
         case "s":
-            var s_button = new Audio("/sounds/tom-1.mp3").play();
+            var s_button = new Audio("/drums_band_with_js/sounds/tom-1.mp3").play();
             break;
         case "d":
-            var d_button = new Audio("/sounds/kick-bass.mp3").play();
+            var d_button = new Audio("/drums_band_with_js/sounds/kick-bass.mp3").play();
             break;
         case "j":
-            var j_button = new Audio("/sounds/tom-2.mp3").play();
+            var j_button = new Audio("/drums_band_with_js/sounds/tom-2.mp3").play();
             break;
         case "k":
-            var k_button = new Audio("/sounds/crash.mp3").play();
+            var k_button = new Audio("/drums_band_with_js/sounds/crash.mp3").play();
             break;
         case "l":
-            var l_button = new Audio("/sounds/tom-4.mp3").play();
+            var l_button = new Audio("/drums_band_with_js/sounds/tom-4.mp3").play();
             break;
         default:
             console.log("press the instruments buttom only!!");
